b5696b8c4260db6982c6f8dff02783e3b76e33aa
yhaenggi <yhaenggi@darkgamex.ch>
2019-09-24 20:58:09 +0200

    apply script

--- apply.sh
+++ apply.sh
@@ -1,3 +1,5 @@
-kubectl delete configmap game-config
-kubectl create configmap game-config --from-file=openttd.cfg
+kubectl delete configmap openttd-config
+kubectl create configmap openttd-config --from-file=openttd.cfg
+kubectl apply -f 05-openttd-pvc.yaml
 kubectl apply -f 10-openttd-deployment.yaml
+kubectl apply -f 20-openttd-svc.yaml
